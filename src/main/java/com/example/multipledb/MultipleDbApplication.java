c5ed4d7599aabfe911087c5d30d152f53c7a6667
sumitjoshi1989 <105304129+sumitjoshi1989@users.noreply.github.com>
2025-03-08 11:18:59 +0530

    random

--- src/main/java/com/example/multipledb/MultipleDbApplication.java
+++ src/main/java/com/example/multipledb/MultipleDbApplication.java
@@ -2,12 +2,23 @@ package com.example.multipledb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class MultipleDbApplication {
+public class MultipleDbApplication  extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+            return application.sources(MultipleDbApplication.class); // Replace DemoApplication with your main class
+    }
+
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(MultipleDbApplication.class, args);
 	}
+	
+	
 
 }
--- src/main/java/com/example/multipledb/ServletInitializer.java
+++ src/main/java/com/example/multipledb/ServletInitializer.java
@@ -1,13 +1,5 @@
 package com.example.multipledb;
 
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-
-public class ServletInitializer extends SpringBootServletInitializer {
-
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-            return application.sources(MultipleDbApplication.class); // Replace DemoApplication with your main class
-    }
+public class ServletInitializer {
 
 }
